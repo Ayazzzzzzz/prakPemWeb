b1675c580edfce42cd5668d5c683b5d049017e33
Ayazzzzzzz <149316954+Ayazzzzzzz@users.noreply.github.com>
2023-11-26 20:46:05 +0700

    Add files via upload

---
+++ project pemweb/selesai.php
@@ -0,0 +1,14 @@
+<?php 
+    include 'connect_pemweb.php';
+
+    $kunjunganID = $_GET['kunjunganID'];
+    // UPDATE `kunjungan` SET `status` = 0 WHERE `kunjungan`.`pasienID` = 2; 
+    $query = mysqli_query($connect, "update kunjungan set status = 1 where kunjunganID = '$kunjunganID'");
+
+    if($query){
+        header("location:appoinment.php");
+    }
+    else {
+        echo "Proses hapus gagal";
+    }
+?>
